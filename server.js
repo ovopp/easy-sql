dde107877ac70b5255a36d850dfd8e954cc5bb91
Vincent <vincentyan8@hotmail.com>
2021-05-30 17:17:52 -0700

    added history list

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var LocalStorage = require('node-localstorage').LocalStorage;
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var localStorage = new LocalStorage('./scratch');
+}
+
 var mysql = require('mysql');
 var con;
 app.set("view engine", "ejs");
@@ -25,27 +30,32 @@ app.post('/database', (req, res) => {
         password: req.body.password,
         database: req.body.database
     });
+    var history_queries = localStorage.getItem("history") == null ? [] : JSON.parse(localStorage.getItem("history"));
 
     con.connect(function (err) {
         if (err) { res.render("home", { result: err }) }
         else {
             console.log("Connected!");
-            res.render("database", { result: [] });
+            res.render("database", { "result": "", "history": history_queries });
         }
     });
 });
 
 app.post('/query', (req, res) => {
-
+    var history_queries = localStorage.getItem("history") == null ? [] : JSON.parse(localStorage.getItem("history"));
     con.query(req.body.query, function (err, result) {
         if (err) {
             console.log(err)
-            res.render("database", { "result": JSON.stringify(err) });
+            res.render("database", { "result": JSON.stringify(err), "history": history_queries });
         }
         else {
             console.log("Query Done");
-
-            res.render("database", { "result": JSON.stringify(result) });
+            if(history_queries.length === 10){
+                history_queries = history_queries.slice(1,10);
+            }
+            history_queries.push(req.body.query);
+            localStorage.setItem("history", JSON.stringify(history_queries));
+            res.render("database", { "result": JSON.stringify(result), "history": history_queries });
         }
     });
 });
